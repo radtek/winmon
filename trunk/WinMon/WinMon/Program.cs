b94d24c43927216f2bfd89b73116ccbaf652dedd
aollesch@gmail.com <aollesch@gmail.com@eee78584-492e-c7fc-eea2-624038a64444>
2012-12-24 20:15:02 +0000

    upload winmon V0.01

---
+++ trunk/WinMon/WinMon/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace WinMon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class tools
+    {
+        static void start_listener()
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://*:8080");
+            listener.Start();
+
+
+
+        }
+
+
+    }
+}
