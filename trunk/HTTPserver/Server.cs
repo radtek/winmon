5bec2969c91a5a2ffc983769e289f5e5befe4d12
aollesch@gmail.com <aollesch@gmail.com@eee78584-492e-c7fc-eea2-624038a64444>
2013-01-30 13:00:26 +0000

    Klassen hinzugefügt

---
+++ trunk/HTTPserver/Server.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Reflection;
+
+namespace HTTPserver
+{
+    public class Server
+    {
+        // öffentliche Felder
+        public String[] http_ports = new String[] { "8080" };
+        public String[] https_ports = new String[] { "4443" };
+        public Boolean enabel_https = false;
+
+        // private Felder
+        private HttpListener listener = new HttpListener();
+        private String root_path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+        // öffentliche Methoden
+        public Server()
+        {
+
+
+        }
+
+        public void start_server()
+        {
+            
+        }
+
+        public void stop_server()
+        {
+
+        }
+
+        // private Methoden
+
+        private void wait_request()
+        {
+            IAsyncResult result = listener.BeginGetContext(callback, listener);
+            result.AsyncWaitHandle.WaitOne();
+        }
+
+
+        private void callback(IAsyncResult result)
+        {
+            if (listener.IsListening)
+            {
+                HttpListenerContext context = listener.EndGetContext(result);
+                HttpListenerRequest request = context.Request;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+
+        private void request_handler(HttpListenerResponse handler_response, HttpListenerRequest handler_request)
+        {
+            string document = handler_request.RawUrl;
+            string docpath = Path.Combine(root_path, document);
+            byte[] responsebyte;
+            if (File.Exists(docpath))
+            {
+                handler_response.StatusCode = (int)HttpStatusCode.OK;
+                responsebyte = File.ReadAllBytes(docpath);
+            }
+            else
+            {
+                handler_response.StatusCode = (int)HttpStatusCode.NotFound;
+                responsebyte = File.ReadAllBytes(root_path + "\\error\\error.html");
+            }
+        }
+    }
+}
